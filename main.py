68de2cdb9b10077c94ce19989e990bae1dcd041c
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2022-04-12 10:27:46 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 stred = [2, 2]
 led.plot_brightness(stred[0], stred[1], 155)
 
-bodA = [0,0]
-bodB = [0,2]
-bodC = [2,0]
+triangle = [[0,1], [1,2], [2,0]]
 
 def rozsvitBody():
-    led.plot(bodA[0], bodA[1])
-    led.plot(bodB[0], bodB[1])
-    led.plot(bodC[0], bodC[1])
+    basic.clear_screen()
+    led.plot_brightness(stred[0], stred[1], 155)
+    for bod in triangle:
+        led.plot(bod[0], bod[1])
 
 rozsvitBody()
+
+def otoc(vpravo):
+    for i in range(0, 3):
+        tempNula = triangle[i][0]
+        if vpravo:
+            triangle[i][0] = -triangle[i][1] + stred[1] + stred[0]
+            triangle[i][1] = tempNula - stred[0] + stred[1]
+        else:
+            triangle[i][0] = triangle[i][1] - stred[1] + stred[0]
+            triangle[i][1] = -tempNula + stred[0] + stred[1]
+    rozsvitBody()
+
+def on_button_pressed_a():
+    otoc(True)
+    #+90°
+input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    otoc(False)
+    #-90°
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -1,12 +1,36 @@
 let stred = [2, 2]
 led.plotBrightness(stred[0], stred[1], 155)
-let bodA = [0, 0]
-let bodB = [0, 2]
-let bodC = [2, 0]
+let triangle = [[0, 1], [1, 2], [2, 0]]
 function rozsvitBody() {
-    led.plot(bodA[0], bodA[1])
-    led.plot(bodB[0], bodB[1])
-    led.plot(bodC[0], bodC[1])
+    basic.clearScreen()
+    led.plotBrightness(stred[0], stred[1], 155)
+    for (let bod of triangle) {
+        led.plot(bod[0], bod[1])
+    }
 }
 
 rozsvitBody()
+function otoc(vpravo: boolean) {
+    let tempNula: number;
+    for (let i = 0; i < 3; i++) {
+        tempNula = triangle[i][0]
+        if (vpravo) {
+            triangle[i][0] = -triangle[i][1] + stred[1] + stred[0]
+            triangle[i][1] = tempNula - stred[0] + stred[1]
+        } else {
+            triangle[i][0] = triangle[i][1] - stred[1] + stred[0]
+            triangle[i][1] = -tempNula + stred[0] + stred[1]
+        }
+        
+    }
+    rozsvitBody()
+}
+
+// +90°
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    otoc(true)
+})
+// -90°
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    otoc(false)
+})
